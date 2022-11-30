94903f3d8825aa2548ae726f81c9542a011e2f96
Diego Guadarrama <65926433+DiegoG8@users.noreply.github.com>
2022-11-29 17:54:27 -0800

    added code given in notebook for superblock

--- src/Superblock.java
+++ src/Superblock.java
@@ -8,6 +8,22 @@ public class Superblock {
 	// you implement
 	public SuperBlock( int diskSize ) {
 		// read the superblock from disk	
+		byte[] superBlock = new byte[Disk.blockSize];
+		SysLib.rawread(0, superBlock);
+		totalBlocks = SysLib.bytes2int(superBlock,0);
+		totalInodes = SysLib.bytes2int(superBlock,4);
+		freeList = SysLib.bytes2int(superBlock,8);
+
+		//validate disk contents
+		if(totalBlocks == diskSize && totalInodes > 0 && freeList >= 2){
+			// disk contents are valid
+			return;
+		}
+		else{
+			// need to format disk
+			totalBlocks = diskSize;
+			format(defaultInodeBlocks);
+		}
 	}
 	
 	//  helper function
