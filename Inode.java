72c4305224fcc47f7ac94b2793e83a341feff0ff
Aditya Duggirala <63118926+aditya655@users.noreply.github.com>
2022-12-14 16:41:25 -0800

    Update Inode.java

--- Inode.java
+++ Inode.java
@@ -1,15 +1,15 @@
 public class Inode {
-    public static final int iNodeSize = 32;
-    public static final int directSize = 11;
+    public static final int iNodeSize = 32; // fix to 32 bytes
+    public static final int directSize = 11; // # direct pointers 
     public static final int IN_USE = -1;
     public static final int EMPTY = 0;
     public static final int AVAILABLE = 1;
-    public int length;
-    public short count;
-    public short flag;
-    public short[] direct = new short[directSize];
-    public short indirect;
-    // constructor for inode
+    public int length; // file size in bytes
+    public short count; // # file-table entries pointing to this
+    public short flag; // 0 = unused, 1 = used, ...
+    public short[] direct = new short[directSize]; // a indirect pointer
+    public short indirect; // a indirect pointer
+    // default constructor 
     public Inode() {
         length = 0;
         count = 0;
@@ -19,7 +19,7 @@ public class Inode {
         }
         indirect = -1;
     }
-    // inode constructor with parameter overide
+    // retrieving inode from disk
     public Inode(short num) {
         int blockNumber = 1 + num / 16;
         byte[] data = new byte[Disk.blockSize];
@@ -37,8 +37,8 @@ public class Inode {
         }
         indirect = SysLib.bytes2short(data, offset);
     }
-    // read/write to disk
-    public void toDisk(short num) {
+    
+    public void toDisk(short num) { // save to disk as the i-th inode
         byte[] data = new byte[iNodeSize];
         int offset = 0;
         SysLib.int2bytes(length, data, offset);
